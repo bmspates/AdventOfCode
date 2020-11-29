2b32f7f18c1c6afb832a6dbd4d14122d62a81a4e
Brian Spates <brianspates@gmail.com>
2020-11-28 23:12:30 -0500

    Added my aoc-dl script

---
+++ aoc-dl.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# Downloads the Advent of Code input file for the current (or other specified)
+# day.
+
+DAY=$1
+YEAR=$2
+SESSION=""
+
+[[ -z "$DAY" ]] && DAY=$(date +%d) || : 
+[[ -z "$YEAR" ]] && YEAR=$(date +%Y) || :
+
+DATE=$(date +%s)
+PUZZLE_DATE=$(date -d "$YEAR"-12-"$DAY" +%s)
+
+if [[ $DATE -lt $PUZZLE_DATE ]]; then
+	echo "Invalid Date! Try again with a published puzzle"
+	exit 1
+fi
+
+curl https://adventofcode.com/"$YEAR"/day/"$DAY"/input --cookie "session=${SESSION}" --output day"$DAY".txt
