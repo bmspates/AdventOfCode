139e0ae414e4854044ebb7c417acd023cabdc1df
Brian Spates <brianspates@gmail.com>
2020-12-04 18:45:11 -0500

    im honestly not sure what I changed so it's probably not important

--- 2019/11.py
+++ 2019/11.py
@@ -10,7 +10,8 @@ def part_two(data):
     pass
 
 
-data = [ line.strip() for line in open("inputs/day_.txt", 'r') ]
+data = [ line.strip() for line in open("inputs/day11.txt", 'r') ]
+data = [ int(num) for num in data[0].split(',') ]
 
 # Data processing here
 
