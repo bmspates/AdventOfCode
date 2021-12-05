0f4073e8193d41d8927186166adebdf084e43a81
Brian Spates <brianspates@gmail.com>
2021-12-05 01:12:49 -0500

    rearrange

--- 2021/05.rkt
+++ 2021/05.rkt
@@ -2,6 +2,11 @@
 (require "input.rkt" "utils.rkt" "line.rkt")
 (provide main)
 
+(define (main)
+  (let ((input (parse-inputs (read-lines "inputs/day5.txt"))))
+    (values (solve input #f)
+            (solve input #t))))
+
 (define (parse-inputs ls)
   (match ls
     ['() '()]
@@ -9,6 +14,14 @@
      (let ([points (map (lambda (x) (string-split x ",")) (string-split l " -> "))])
        (cons (Line (make-point (car points)) (make-point (cadr points)))
              (parse-inputs ls)))]))
+
+(define (solve input diag?)
+  (let ([h (make-hash)])
+    (begin
+      (for/list ([line input])
+        (add-points line h diag?))
+      (count-overlap h 2))))
+
 
 (define (add-points line h diag?)
   (if (not (or (op-x line =) (op-y line =) diag?))
@@ -20,19 +33,7 @@
             (begin (hash-update! h p1 add1 0)
                    (add-points (next-point line) h diag?))))))
 
-(define (main)
-  (let ((input (parse-inputs (read-lines "inputs/day5.txt"))))
-    (values (solve input #f)
-            (solve input #t))))
-
 (define (count-overlap h n)
   (foldl (lambda (x res) (if (<= n x) (add1 res) res))
          0
          (hash-values h)))
-
-(define (solve input diag?)
-  (let ([h (make-hash)])
-    (begin
-      (for/list ([line input])
-        (add-points line h diag?))
-      (count-overlap h 2))))
